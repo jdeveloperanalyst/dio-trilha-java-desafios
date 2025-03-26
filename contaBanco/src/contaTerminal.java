cac38470d9f3d222a836610de42b2774e9dd59a5
jdeveloperanalyst <jonatas1200@icloud.com>
2025-03-26 20:40:11 -0300

    feature/desafio-sintaxe-conta-terminal

---
+++ contaBanco/src/contaTerminal.java
@@ -0,0 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class contaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Informe a Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o Nome do Cliente!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Informe o Saldo!");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
