f300ea60bfd55854f69e809b6b2be1a5f02f0128
jdeveloperanalyst <jonatas1200@icloud.com>
2025-04-01 20:06:24 -0300

    update

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,17 +1,19 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);;
 
         System.out.println("Por favor, digite o número da Conta!");
         int numero = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.println("Informe a Agência!");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         System.out.println("Informe o Nome do Cliente!");
-        String nomeCliente = scanner.next();
+        String nomeCliente = scanner.nextLine();
 
         System.out.println("Informe o Saldo!");
         float saldo = scanner.nextFloat();
