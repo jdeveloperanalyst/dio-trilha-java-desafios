d18141b655578056c5f4aad9a13dd09035046511
jdeveloperanalyst <jonatas1200@icloud.com>
2025-06-08 20:45:19 -0300

    new-feature/desafio-catalogo-de-livros

---
+++ collections-java-api-2025/src/dev/jon/desafio/list/operacoesbasicas/catalogodelivros/CatalogoApp.java
@@ -0,0 +1,21 @@
+package dev.jon.desafio.list.operacoesbasicas.catalogodelivros;
+
+public class CatalogoApp {
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Lei da Atração", "Michael J Losier", 2004);
+        catalogoLivros.adicionarLivro("Imigrante Ilegal", "Raiam Santos", 2014);
+        catalogoLivros.adicionarLivro("Primeiro Milhão", "Raiam Santos", 2023);
+        catalogoLivros.adicionarLivro("Segundo Milhão", "Raiam Santos", 2025);
+        catalogoLivros.adicionarLivro("Primeiro Milhão", "Elon Musk", 2006);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Raiam Santos"));
+
+        System.out.println(catalogoLivros.pesquisarPorIntervalo(2006, 2023));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Primeiro Milhão"));
+
+        catalogoLivros.exibirCatalogo();
+    }
+}
